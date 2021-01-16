445367d010240a1855de550631a5f36e1d5b22fb
messiashub <claudineimessias@hotmail.com>
2021-01-16 01:06:59 -0300

    criando app react

---
+++ my-app2/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>Teste</p>
+      Olá Mundo
+    </div>
+    
+  );
+}
+
+export default App;
